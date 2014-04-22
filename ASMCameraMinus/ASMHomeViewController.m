bee36c0db6b172727bb3dd4e48c3a71d657b3360
Jorge Marcos <jorge.marcos@live.u-tad.com>
2014-04-22 13:58:32 +0200

    Good news: my biggest mistake was that I was using another UICollectionView that I declared in the ".m" rather than the one of the XIB. Also, the array wasn't mutable. We've done some other things, but the code's a little sloppy, so stay tuned for our next commit :)

--- ASMCameraMinus/ASMHomeViewController.m
+++ ASMCameraMinus/ASMHomeViewController.m
@@ -9,9 +9,7 @@
 #import "ASMHomeViewController.h"
 #import "ASMPhotoCell.h"
 
-@interface ASMHomeViewController () {
-//    UICollectionView *myPhotosCV;
-}
+@interface ASMHomeViewController ()
 
 @property (strong, nonatomic) NSMutableArray *myPhotosArray;
 
@@ -38,6 +36,10 @@
     self.photosCV.dataSource = self;
     
     self.myPhotosArray = [[NSMutableArray alloc]init];
+    [self.myPhotosArray addObject:[UIImage imageNamed:@"c3po.jpg"]];
+    [self.myPhotosArray addObject:[UIImage imageNamed:@"candemor.jpg"]];
+    [self.myPhotosArray addObject:[UIImage imageNamed:@"chewbacca.jpg"]];
+    [self.myPhotosArray addObject:[UIImage imageNamed:@"darthVader.jpg"]];
     
     [self.photosCV registerNib:[UINib nibWithNibName:@"ASMPhotoCell" bundle:nil] forCellWithReuseIdentifier:@"PhotoCell"];
 }
@@ -56,10 +58,13 @@
 
 - (IBAction)shoot:(id)sender
 {
-    UIImagePickerController *pc_shoot = [[UIImagePickerController alloc] init];
-    pc_shoot.delegate = self;
-    pc_shoot.sourceType = UIImagePickerControllerSourceTypeCamera;
-    [self presentViewController:pc_shoot animated:YES completion:nil];
+    if ( [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] )
+    {
+        UIImagePickerController *pc_shoot = [[UIImagePickerController alloc] init];
+        pc_shoot.delegate = self;
+        pc_shoot.sourceType = UIImagePickerControllerSourceTypeCamera;
+        [self presentViewController:pc_shoot animated:YES completion:nil];
+    }
     
     // okay, we really don't need any of this code right here...
 //    // this checks if the device has a camera
@@ -89,7 +94,7 @@
     }
 }
 
-#pragma mark - delegate methods
+#pragma mark - picker view delegate methods
 
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
@@ -100,7 +105,14 @@
     [self.photosCV reloadData];
 }
 
-#pragma mark - collection view methods
+#pragma mark - collection view flow layout delegate methods
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(61, 62);
+}
+
+#pragma mark - collection view data source delegate methods
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
@@ -118,7 +130,37 @@
     
     cell.image.image = [self.myPhotosArray objectAtIndex:indexPath.item];
     
+//    if( cell.selected )
+//    {
+//        cell.backgroundColor = [UIColor orangeColor];
+//    }
+//    else
+//    {
+//        cell.backgroundColor = [UIColor blackColor];
+//    }
+    cell.backgroundColor = ( cell.selected ) ? [UIColor orangeColor] : [UIColor blackColor];
+    
     return cell;
+}
+
+#pragma mark - collection view delegate methods
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    long sectionID = indexPath.section;
+    long itemID = indexPath.item;
+    
+    UIImage *selectedImage = [self.myPhotosArray objectAtIndex:indexPath.item];
+    self.imageView.image = selectedImage;
+    
+    UICollectionViewCell* cell = [self.photosCV cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didDeselectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.photosCV cellForItemAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor blackColor];
 }
 
 @end
