e59cee5a6ad848264ec06a7431899a81299171a0
Jorge Marcos <jorge.marcos@live.u-tad.com>
2014-05-08 00:24:40 +0200

    Recreación del xib de la celda

---
+++ ASMCameraMinus/ASMTableViewCell.h
@@ -0,0 +1,26 @@
+/***************************************************************************/
+/*                                                                         */
+/*  ASMTableViewCell.h                                                         */
+/*  Copyright (c) 2014 SoReT. All rights reserved.                       */
+/*                                                                         */
+/*  Description: ASMCameraMinus                                         */
+/*               ASMTableViewCell Interface Class            */
+/*                                                                         */
+/*       Author: Jorge Marcos Fernandez                                    */
+/*                                                                         */
+/***************************************************************************/
+#import <UIKit/UIKit.h>
+
+
+/***************************************************************************/
+/*                                                                         */
+/*                                                                         */
+/*                                                                         */
+/*  ASMTableViewCell Interface Definition                    */
+/*                                                                         */
+/*                                                                         */
+/*                                                                         */
+/***************************************************************************/
+@interface ASMTableViewCell : UITableViewCell
+
+@end
---
+++ ASMCameraMinus/ASMTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  ASMTableViewCell.m
+//  ASMCameraMinus
+//
+//  Created by Jorge Marcos Fernandez on 08/05/14.
+//  Copyright (c) 2014 SoReT. All rights reserved.
+//
+
+#import "ASMTableViewCell.h"
+
+@implementation ASMTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
